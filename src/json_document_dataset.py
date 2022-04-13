7ed9da4abaa249992705dab7fdef761c34b403b3
Kamil Bujel <kamilbujel@gmail.com>
2022-04-13 12:59:49 +0200

    update logging

--- src/json_document_dataset.py
+++ src/json_document_dataset.py
@@ -44,7 +44,8 @@ class JsonDocumentDataset(Dataset):
             data_dict = json.load(f)
 
         # Check generic format of the input
-        logger.info(f"columns: {set(data_dict["documents"][0].keys()}")
+        keys = set(data_dict["documents"][0].keys())
+        logger.info(f"columns: {keys}")
         assert "documents" in data_dict.keys()
         assert {"tokens", "document_label", "sentence_labels", "token_labels"} == set(data_dict["documents"][0].keys())
 
