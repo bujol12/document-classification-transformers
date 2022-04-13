fix crossentropy to operate on logits and not softmax

src/document_model.py
@@ -1,7 +1,7 @@
 import torch
 import logging
 
-from transformers import AutoModel, PretrainedConfig, AutoConfig, PreTrainedModel
+from transformers import AutoModel, PretrainedConfig, AutoConfig, PreTrainedModel, AutoModelForSequenceClassification
 from transformers.modeling_outputs import BaseModelOutput
 
 from .config import Config
@@ -35,8 +35,13 @@ class DocumentModel(torch.nn.Module):
             config=self.lm_config,
         )
 
+        self.cls_hidden = torch.nn.Linear(self.lm_config.hidden_size, self.lm_config.hidden_size)
+        self.cls_dropout = torch.nn.Dropout(p=self.config.dropout)
         self.cls_logit_layer = torch.nn.Linear(self.lm_config.hidden_size, self.config.num_labels)
-        self.cls_softmax = torch.nn.Softmax(dim=-1)
+
+        self.__init_weights(self.cls_hidden)
+        self.__init_weights(self.cls_dropout)
+        self.__init_weights(self.cls_logit_layer)
 
     def forward(self, input_ids, attention_mask, token_type_ids=None, position_ids=None, head_mask=None,
                 inputs_embeds=None, **kwargs):
@@ -54,9 +59,11 @@ class DocumentModel(torch.nn.Module):
             inputs_embeds=inputs_embeds,
         )  # last_hidden_state, hidden_states, attentions
 
-        cls_probs = self.cls_softmax(self.cls_logit_layer(self.lm_outputs.last_hidden_state[:, 0]))
+        cls_hidden = self.cls_hidden(self.lm_outputs.pooler_output)  # last_hidden_state[:, 0])
+        logits = self.cls_logit_layer(self.cls_dropout(cls_hidden))
         token_outputs = self.lm_outputs.last_hidden_state[:, 1:]
-        return cls_probs, token_outputs
+
+        return logits, token_outputs
 
     @staticmethod
     def get_transformers_config(config: Config) -> PretrainedConfig:
@@ -67,3 +74,15 @@ class DocumentModel(torch.nn.Module):
         :return: Transformers configs
         """
         return AutoConfig.from_pretrained(config.transformers_model_name_or_path, **config.transformers_override)
+
+    def __init_weights(self, m):
+        if self.config.initializer_name == "normal":
+            self.initializer = torch.nn.init.normal_
+        elif self.config.initializer_name == "glorot":
+            self.initializer = torch.nn.init.xavier_normal_
+        elif self.config.initializer_name == "xavier":
+            self.initializer = torch.nn.init.xavier_uniform_
+
+        if isinstance(m, torch.nn.Linear):
+            self.initializer(m.weight)
+            torch.nn.init.zeros_(m.bias)