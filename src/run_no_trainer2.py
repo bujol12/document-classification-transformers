57f0131f24365b9a5cd842d437bc5d830a2389c0
Kamil Bujel <kamilbujel@gmail.com>
2022-04-28 18:41:29 +0100

    no trainer all params

--- src/run_no_trainer2.py
+++ src/run_no_trainer2.py
@@ -375,7 +375,8 @@ def main():
             "weight_decay": 0.0,
         },
     ]
-    optimizer = AdamW(optimizer_grouped_parameters, lr=args.learning_rate)
+    # optimizer = AdamW(optimizer_grouped_parameters, lr=args.learning_rate)
+    optimizer = AdamW(model.parameters(), lr=args.learning_rate)
 
     # Scheduler and math around the number of training steps.
     num_update_steps_per_epoch = math.ceil(len(train_dataloader) / args.gradient_accumulation_steps)
