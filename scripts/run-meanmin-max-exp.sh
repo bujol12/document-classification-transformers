d6ac1ec1f6b6a5ee61092588409aea1e31b19969
Kamil Bujel <kamilbujel@gmail.com>
2022-06-06 19:21:44 +0100

    meanminmax

---
+++ scripts/run-meanmin-max-exp.sh
@@ -0,0 +1,12 @@
+# Run Min-Max For all datasets and both longformer and roberta
+
+python main.py --config config/soft_attention/min_max_mean/roberta-fce.json --train data/processed/fce/json/train.json  --eval data/processed/fce/json/dev.json
+python main.py --config config/soft_attention/min_max_mean/roberta-bea2019.json --train data/processed/bea2019/json/train.json  --eval data/processed/bea2019/json/dev.json
+python main.py --config config/soft_attention/min_max_mean/roberta-pos_imdb.json --train data/processed/imdb/json/pos_train.json  --eval data/processed/imdb/json/pos_dev.json
+python main.py --config config/soft_attention/min_max_mean/roberta-neg_imdb.json --train data/processed/imdb/json/neg_train.json  --eval data/processed/imdb/json/neg_dev.json
+
+
+python main.py --config config/soft_attention/min_max_mean/longformer-fce.json --train data/processed/fce/json/train.json  --eval data/processed/fce/json/dev.json
+python main.py --config config/soft_attention/min_max_mean/longformer-bea2019.json --train data/processed/bea2019/json/train.json  --eval data/processed/bea2019/json/dev.json
+python main.py --config config/soft_attention/min_max_mean/longformer-pos_imdb.json --train data/processed/imdb/json/pos_train.json  --eval data/processed/imdb/json/pos_dev.json
+python main.py --config config/soft_attention/min_max_mean/longformer-neg_imdb.json --train data/processed/imdb/json/neg_train.json  --eval data/processed/imdb/json/neg_dev.json
