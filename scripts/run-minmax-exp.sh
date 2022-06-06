2583e95577d17ce49d7c7b4cff860d99ed7a1ec7
Kamil Bujel <kamilbujel@gmail.com>
2022-06-06 20:30:36 +0100

    change scripts for experiments to be only for roberta and not longformer

--- scripts/run-minmax-exp.sh
+++ scripts/run-minmax-exp.sh
@@ -4,9 +4,3 @@ python main.py --config config/soft_attention/min_max/roberta-fce.json --train d
 python main.py --config config/soft_attention/min_max/roberta-bea2019.json --train data/processed/bea2019/json/train.json  --eval data/processed/bea2019/json/dev.json
 python main.py --config config/soft_attention/min_max/roberta-pos_imdb.json --train data/processed/imdb/json/pos_train.json  --eval data/processed/imdb/json/pos_dev.json
 python main.py --config config/soft_attention/min_max/roberta-neg_imdb.json --train data/processed/imdb/json/neg_train.json  --eval data/processed/imdb/json/neg_dev.json
-
-
-python main.py --config config/soft_attention/min_max/longformer-fce.json --train data/processed/fce/json/train.json  --eval data/processed/fce/json/dev.json
-python main.py --config config/soft_attention/min_max/longformer-bea2019.json --train data/processed/bea2019/json/train.json  --eval data/processed/bea2019/json/dev.json
-python main.py --config config/soft_attention/min_max/longformer-pos_imdb.json --train data/processed/imdb/json/pos_train.json  --eval data/processed/imdb/json/pos_dev.json
-python main.py --config config/soft_attention/min_max/longformer-neg_imdb.json --train data/processed/imdb/json/neg_train.json  --eval data/processed/imdb/json/neg_dev.json
