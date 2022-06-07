5c543f8dd5f2f25d73bfee694d538ac892df9161
Kamil Bujel <kamilbujel@gmail.com>
2022-06-07 17:06:58 +0100

    Run longformer minmax trial #1 on Slurm

--- scripts/run-minmax-exp.sh
+++ scripts/run-minmax-exp.sh
@@ -1,11 +1,11 @@
 # Run Min-Max For all datasets and both longformer and roberta
 
-python main.py --config config/soft_attention/min_max/roberta-fce.json --train data/processed/fce/json/train.json  --eval data/processed/fce/json/dev.json --test data/processed/fce/json/test.json
-python main.py --config config/soft_attention/min_max/roberta-bea2019.json --train data/processed/bea2019/json/train.json  --eval data/processed/bea2019/json/dev.json --test data/processed/bea2019/json/test.json
-python main.py --config config/soft_attention/min_max/roberta-pos_imdb.json --train data/processed/imdb/json/pos_train.json  --eval data/processed/imdb/json/pos_dev.json --test data/processed/imdb/json/pos_test.json
-python main.py --config config/soft_attention/min_max/roberta-neg_imdb.json --train data/processed/imdb/json/neg_train.json  --eval data/processed/imdb/json/neg_dev.json --test data/processed/imdb/json/neg_test.json
+#python main.py --config config/soft_attention/min_max/roberta-fce.json --train data/processed/fce/json/train.json  --eval data/processed/fce/json/dev.json --test data/processed/fce/json/test.json
+#python main.py --config config/soft_attention/min_max/roberta-bea2019.json --train data/processed/bea2019/json/train.json  --eval data/processed/bea2019/json/dev.json --test data/processed/bea2019/json/test.json
+#python main.py --config config/soft_attention/min_max/roberta-pos_imdb.json --train data/processed/imdb/json/pos_train.json  --eval data/processed/imdb/json/pos_dev.json --test data/processed/imdb/json/pos_test.json
+#python main.py --config config/soft_attention/min_max/roberta-neg_imdb.json --train data/processed/imdb/json/neg_train.json  --eval data/processed/imdb/json/neg_dev.json --test data/processed/imdb/json/neg_test.json
 
-python main.py --config config/soft_attention/min_max/longformer_fce.json --train data/processed/fce/json/train.json  --eval data/processed/fce/json/dev.json --test data/processed/fce/json/test.json
-python main.py --config config/soft_attention/min_max/longformer_bea2019.json --train data/processed/bea2019/json/train.json  --eval data/processed/bea2019/json/dev.json --test data/processed/bea2019/json/test.json
+python main.py --config config/soft_attention/min_max/longformer-fce.json --train data/processed/fce/json/train.json  --eval data/processed/fce/json/dev.json --test data/processed/fce/json/test.json
+python main.py --config config/soft_attention/min_max/longformer-bea2019.json --train data/processed/bea2019/json/train.json  --eval data/processed/bea2019/json/dev.json --test data/processed/bea2019/json/test.json
 python main.py --config config/soft_attention/min_max/longformer-pos_imdb.json --train data/processed/imdb/json/pos_train.json  --eval data/processed/imdb/json/pos_dev.json --test data/processed/imdb/json/pos_test.json
 python main.py --config config/soft_attention/min_max/longformer-neg_imdb.json --train data/processed/imdb/json/neg_train.json  --eval data/processed/imdb/json/neg_dev.json --test data/processed/imdb/json/neg_test.json
