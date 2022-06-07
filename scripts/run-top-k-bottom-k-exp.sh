b66d46b0f6a8e4f57376f9e58173e94eab8daf73
Kamil Bujel <kamilbujel@gmail.com>
2022-06-07 22:13:20 +0100

    topkbottomk

---
+++ scripts/run-top-k-bottom-k-exp.sh
@@ -0,0 +1,6 @@
+# Run Min-Max For all datasets and both longformer and roberta
+
+python main.py --config config/soft_attention/top_k_bottom_k/roberta-fce.json --train data/processed/fce/json/train.json  --eval data/processed/fce/json/dev.json
+python main.py --config config/soft_attention/top_k_bottom_k/roberta-bea2019.json --train data/processed/bea2019/json/train.json  --eval data/processed/bea2019/json/dev.json
+python main.py --config config/soft_attention/top_k_bottom_k/roberta-pos_imdb.json --train data/processed/imdb/json/pos_train.json  --eval data/processed/imdb/json/pos_dev.json
+python main.py --config config/soft_attention/top_k_bottom_k/roberta-neg_imdb.json --train data/processed/imdb/json/neg_train.json  --eval data/processed/imdb/json/neg_dev.json
